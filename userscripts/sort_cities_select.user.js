cc9ca16bf33c549412509748a2f3c30ea36a2246
Antonio Valenti <valentiantonio89@gmail.com>
2021-02-18 22:13:32 +0100

    US | updates version

--- userscripts/sort_cities_select.user.js
+++ userscripts/sort_cities_select.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sort cities select
 // @namespace    March of history
-// @version      0.1
+// @version      0.1.1
 // @description  Sort the cities list on select, based on population or priority/alphabetical
 // @author       avalenti89
 // @match        http://www.marchofhistory.com/EcranPrincipal.php
